033dac1ee119c99c115e2d0cdb534f36f7ad22ae
Annie <anniezhang848@gmail.com>
2023-11-30 13:48:05 -0500

    Update patient.h
    - Added p_count to automatically number new patients
    - Changed age variable to bool isAdult 
    - Added a status variable to encapsulate both isAlive and isShockable variables
    - Added appropriate getters and setters

--- patient.h
+++ patient.h
@@ -1,22 +1,34 @@
 #ifndef PATIENT_H
 #define PATIENT_H
 
-#include <map>
 #include <QtGlobal>
 #include <QString>
+#include <QObject>
 
 using namespace std;
 
-class Patient
-{
-public:
-    Patient(QString name, int age, int healthStatus);
-    bool isAnAdult();
-private:
-    static map<int, string> healthCondition;
-    QString name;
-    int age;
-    int healthStatus;
+class Patient : public QObject{
+
+    Q_OBJECT
+
+    public:
+        //Public static variable
+        static int p_count;
+
+        //Constructor/destructor
+        Patient(bool isAdult, bool shockable, QObject *parent = nullptr);
+        ~Patient();
+
+        //Getters
+        bool getIsAdult();
+        int getStatus();
+
+        //Setters
+        void setStatus(int newStatus);
+    private:
+        int num;
+        bool isAdult;
+        int status;
 };
 
 #endif // PATIENT_H
